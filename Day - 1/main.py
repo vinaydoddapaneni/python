a3e89e92f9d401b79cd20afb118560a6ebb4d314
dvsmaxv <dvsmaxv@gmail.com>
2021-05-23 16:03:17 -0600

    input function added into the main python file

--- Day - 1/main.py
+++ Day - 1/main.py
@@ -2,7 +2,12 @@
 print("hello ,\n, Vinay")
 print('apple\t\torange')"""
 
-#print("Hello World!\nHello World!\nHello World!")
-#print('Hello'+' Vinay')
+"""print("Hello World!\nHello World!\nHello World!")
+print('Hello'+' Vinay')"""
 
 # Input Function
+
+name = input("What's your name\n:")
+print('Welcome '+name)
+
+print("Hello "+input("What is your name?\n: "))
