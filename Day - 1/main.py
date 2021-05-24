b115030f6d1d85573f52b90f56b8f70ed71b6278
dvsmaxv <dvsmaxv@gmail.com>
2021-05-24 14:24:37 -0600

    I finised today's project and also I did learned more about variable

--- Day - 1/main.py
+++ Day - 1/main.py
@@ -7,7 +7,17 @@ print('Hello'+' Vinay')"""
 
 # Input Function
 
-name = input("What's your name\n:")
+"""name = input("What's your name\n:")
 print('Welcome '+name)
 
-print("Hello "+input("What is your name?\n: "))
+print("Hello "+input("What is your name?\n: "))"""
+
+#Initialate the value through the variable
+"""name = input("What is you name?\n")
+print("My name is "+name)"""
+
+# Final project of the Day
+print("Welcome to band name generator")
+city = input("Which city did you grew up? ")
+pet = input("What is your pen name? ")
+print("Your band name would be: "+city+" "+pet)
